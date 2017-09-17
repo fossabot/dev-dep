1d57572a8d7f2c4d06b3a88b5ea7f577f9f2802d
dr-js <dr@dr.city>
2017-09-17 14:33:41 +0800

    update checkOutdated

--- source/index.js
+++ source/index.js
@@ -18,21 +18,18 @@ const writeFileAsync = promisify(nodeModuleFs.writeFile)
 const { Format, Mutable: { MutableOperation: { objectMergeDeep } } } = Common
 const {
   Module: { runCommand, withCwd },
-  File: { FILE_TYPE, getDirectoryContent, walkDirectoryContentShallow, createDirectory, modify }
+  File: { FILE_TYPE, getDirectoryContent, walkDirectoryContent, createDirectory, modify }
 } = Node
-const getDirectoryContentShallow = (path, pathType) => getDirectoryContent(path, pathType, true)
 
 const PATH_RESOURCE = nodeModulePath.resolve(__dirname, '../resource')
 
 // check
-const checkOutdated = async () => walkDirectoryContentShallow(
-  await getDirectoryContentShallow(PATH_RESOURCE),
+const checkOutdated = async () => walkDirectoryContent(
+  await getDirectoryContent(PATH_RESOURCE),
   async (path, name, fileType) => {
     if (fileType !== FILE_TYPE.Directory) return
+    if (!nodeModuleFs.existsSync(nodeModulePath.join(path, name, 'package.json'))) return
     console.log(`[checkOutdated] checking '${nodeModulePath.join(path, name)}'`)
-    // let resultObject
-    // try { resultObject = await withCwd(nodeModulePath.join(path, name), runCommand)('npm outdated') } catch (error) { resultObject = error }
-    // const { stdoutString, stderrString } = resultObject
     const { code, status, stdoutString, stderrString } = await withCwd(nodeModulePath.join(path, name), runCommand)('npm outdated').catch((error) => error)
     code && console.log(Format.stringIndentLine(`code: ${code}, status: ${status}`, '  '))
     stdoutString && console.log(Format.stringIndentLine(stdoutString, '  '))
